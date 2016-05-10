5184eeda48f42a7e5fe1c0add73b98e3427ff8cc
Chris Cook <chris@darious.co.uk>
2016-05-10 06:39:27 +0100

    Minor tweaks

--- hevcit_win/allpath_calc.sh
+++ hevcit_win/allpath_calc.sh
@@ -8,8 +8,8 @@ echo "$TempScriptName"
 
 rm "$TempScriptName"
 
-find "$path" -type f \( -iname \*.mp4 -o -iname \*.mkv -o -iname \*.m4v -o -iname \*.avi -o -iname \*.mov \) | sort | while read line; do
-	echo -e "\e[43m\e[30m./hevcit.sh -b half -f backup '$line'\e[0m"
+find "$path" -type f \( -iname \*.mp4 -o -iname \*.mkv -o -iname \*.m4v -o -iname \*.avi -o -iname \*.mov -o -iname \*.flv \) | sort | while read line; do
+	echo -e "\e[43m\e[30m./hevcit.sh -b calc -f backup '$line'\e[0m"
 	echo "./hevcit.sh -b calc -f backup \"$line\"" >> $TempScriptName
 done;
 
