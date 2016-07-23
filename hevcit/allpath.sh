a22500097443628a5c24389f66a91a4d81eee43b
chris cook <chris@darious.co.uk>
2016-07-23 07:30:10 +0100

    Added parameters to allpath

--- hevcit/allpath.sh
+++ hevcit/allpath.sh
@@ -1,16 +1,31 @@
-path=$1
-shift
+
+while getopts "b:h:f:" OPTION
+do
+    case $OPTION in
+        b)
+            bitrate=$OPTARG
+            ;;
+        h)
+            option_h=$OPTARG
+            ;;
+		f)
+            filepath=$OPTARG
+            ;;
+    esac
+done
+
+
 
 uuid=$(uuidgen)
 TempScriptName="/tmp/hevcit_batch_$uuid.sh"
 
 echo "$TempScriptName"
 
-rm "$TempScriptName" > /dev/null
+rm "$TempScriptName"
 
-find "$path" -type f \( -iname \*.mp4 -o -iname \*.mkv -o -iname \*.m4v -o -iname \*.avi -o -iname \*.mov \) | sort | while read line; do
-	echo -e "\e[43m./hevcit.sh -b half -f backup '$line'\e[0m"
-	echo "./hevcit.sh -b half -f backup \"$line\"" >> $TempScriptName
+find "$filepath" -type f \( -iname \*.mp4 -o -iname \*.mkv -o -iname \*.m4v -o -iname \*.avi -o -iname \*.mov -o -iname \*.flv -o -iname \*.wmv  \) | sort | while read line; do
+	echo -e "\e[43m\e[30m./hevcit.sh -b calc -f backup '$line'\e[0m"
+	echo "./hevcit.sh -b $bitrate -h $option_h -f \"$line\"" >> $TempScriptName
 done;
 
 chmod +x "$TempScriptName"
