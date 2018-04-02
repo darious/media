5b7f42a34c88f8d392282eadd331d22a741d4ff1
Chris <chris@darious.co.uk>
2018-04-02 16:15:42 +0100

    Initial version 2

---
+++ hevcit/recode2.py
@@ -0,0 +1,217 @@
+
+"""
+recode v2.py
+
+Ver  Date       Author      Comment
+2.00 2018-04-02 Chris Cook  Recoded to be more pythonic and split into modules
+
+"""
+
+import sys
+import os
+import argparse
+import base64
+import threading
+import subprocess
+
+# import mediainfo
+from pymediainfo import MediaInfo
+
+# import the core functions lib
+import core_functions as core
+import video_functions as video
+
+# store some constants
+TmpDir = 'H:/Video/temp/'
+BackupDir = '//tank03/backup/video/'
+LowBitRate = 500
+
+
+# pick up the cli arguments
+cli_parser = argparse.ArgumentParser(description='Recodes video into either h264 or h265 with various options')
+cli_parser.add_argument('-b','--bitrate', metavar='<bitrate>', help='Bitrate to encode to, use calc, calc2 or calc3 to set automatically', required=True)
+cli_parser.add_argument('-a','--audio', metavar='<audio>', help='Audio processing to use, passall, all, one, passbest, aac or 64k', required=True)
+cli_parser.add_argument('-v','--video', metavar='<video>', help='Video codec to use, h264 or h265, defaults to h265 if not supplied', required=False, default='h265')
+cli_parser.add_argument('-r','--rescale', metavar='<rescale>', help='Width to rescale the video to e.g. 1280', required=False)
+cli_parser.add_argument('-t','--test', metavar='<test>', help='Duration of video to use as test in seconds, e.g. 60', required=False)
+cli_parser.add_argument('-p','--printmode', help='Print the recode command, do not run it', required=False,action='store_true')
+cli_parser.add_argument('-s','--process', metavar='<process>', help='Method to be used to process the file, new, backup, replace', required=True)
+cli_parser.add_argument('-f','--file', metavar='<file>', help='File or folder to process', required=True)
+cli_parser.add_argument('-d','--debug', help='Debug mode, very verbose', required=False,action='store_true')
+cli_args = cli_parser.parse_args()
+
+
+# setup a logger
+logger=core.GetLogger(debug=cli_args.debug)
+
+# debug log all the cli args
+for arg in vars(cli_args):
+    logger.debug("Cli argument - %s: %s", arg.ljust(10),getattr(cli_args, arg))
+
+logger.debug("Dir to use as Temp = %s", TmpDir)
+logger.debug("Dir to use as backup = %s", BackupDir)
+logger.debug("Min BitRate set as = %s", LowBitRate)
+
+logger.info("Will encode '%s' into %s using bitrate '%s' ,will process the audio as %s and will use a %s file", cli_args.file, cli_args.video, cli_args.bitrate, cli_args.audio, cli_args.process)
+
+
+def RecodeFile(filename):
+    # will turn this into a procedure so that we can process folders as well as files
+    logger.info("Starting processing on '%s'", filename)
+
+    # grab the file extension
+    FileExt = os.path.splitext(filename)[1]
+
+    VideoInfo, AudioInfo, SubInfo, AllInfo = video.GetVideoInfo(filename)
+
+    # some debug logging
+    logger.debug("VideoInfo : %s", VideoInfo)
+    logger.debug("AudioInfo : %s", AudioInfo)
+    logger.debug("SubInfo   : %s", SubInfo)
+    logger.debug("AllInfo   : %s", AllInfo)
+
+    # are we gonna rescale
+    if cli_args.rescale <> None and cli_args.bitrate <> "pass":
+        ffRescale, NewWidth = video.VideoRescaleCalc(VideoInfo[0]['Width'], VideoInfo[0]['Height'], cli_args.rescale)
+    else:
+        NewWidth = VideoInfo[0]['Width']
+
+    mapVid, ffVid, NewBitrate = video.VideoParameters(VideoInfo, cli_args.bitrate, cli_args.video, AllInfo, LowBitRate)
+
+    # some debug logging
+    logger.debug("mapVid     : %s", mapVid)
+    logger.debug("ffVid      : %s", ffVid)
+    logger.debug("NewBitrate : %s", NewBitrate)
+
+    # check work is required
+    workreq = 0
+    if VideoInfo[0]['Format'] <> 'HEVC':
+        workreq=1
+        workreas='video is not encoded in HEVC'
+    elif int(NewWidth) < int(VideoInfo[0]['Width']):
+        workreq=1
+        workreas='video will be recaled to smaller size'
+    elif (NewBitrate / VideoInfo[0]['BitRate']) < 0.93:
+        workreq=1
+        workreas='video will be encoded to a lower bitrate'
+
+    if workreq != 1:
+        logger.warning("No work required on %s", filename)
+    else:
+        logger.info("Recoding as %s", workreas)
+
+        mapAud, ffAud, formatAud = video.AudioParameters(AudioInfo, FileExt, cli_args.audio, AllInfo)
+
+        # some debug logging
+        logger.debug("mapAud    : %s", mapAud)
+        logger.debug("ffAud     : %s", ffAud)
+        logger.debug("formatAud : %s", formatAud)
+
+        # work out what to do with the subs
+        if FileExt == '.avi':
+            logger.info("AVI file so skipping subs")
+            mapSub = []
+            ffSub = []
+            formatSub = []
+        else:		
+            mapSub, ffSub, formatSub= video.SubParameters(SubInfo)
+            # some debug logging
+            logger.debug("mapSub    : %s", mapSub)
+            logger.debug("ffSub     : %s", ffSub)
+            logger.debug("formatSub : %s", formatSub)
+
+        # work out the final file extension
+        if formatAud == 'mkv' or formatSub == 'mkv':
+            format = 'mkv'
+        else:
+            format = 'mp4'
+
+        # PreProcess the files, create backups etc.
+        #VidFileIn, VidFileOut = FilePreProcess (VidFileInWin, fileProcess, format)
+
+        if cli_args.process == 'backup':
+        # then backup the file
+            VidFileBackup = BackupDir + os.path.basename(filename)
+            try:
+                os.remove(VidFileBackup)
+            except OSError:
+                pass
+
+            if cli_args.printmode != True:
+                logger.info("Backing up file from %s to %s", filename, VidFileBackup)
+                core.copy_large_file(filename, VidFileBackup)
+                os.remove(filename)
+                logger.info("Backup from from %s to %s complete", filename, VidFileBackup)
+            else:
+                logger.info("Print mode : File will not be moved")
+
+            # stash the filenames
+            VidFileIn = VidFileBackup
+            VifFileOt = filename
+
+        elif cli_args.process == 'new':
+            VidFileIn = filename
+            VifFileOt = os.path.splitext(VidFileIn)[0]
+            VifFileOt = VifFileOt + '_new' + '.' + format
+
+
+        elif cli_args.process == 'replace':
+            tmpRandom = base64.b64encode(os.urandom(12), '__')
+            VidFileIn = os.path.splitext(filename)[0] + '_' + tmpRandom + os.path.splitext(filename)[1]
+            VifFileOt = os.path.splitext(filename)[0] + '.' + format
+            if cli_args.printmode != True:
+                logger.info("Renaming file from %s to %s", VidFileIn, VifFileOt)
+                os.rename(filename, VidFileIn)
+            else:
+                logger.info("Print mode : File will not be renamed")
+        else:
+            logger.critical("File process method of %s is not understood, exiting", cli_args.process)
+            sys.exit(2)
+
+        # some debug logging
+        logger.debug("VidFileIn : %s", VidFileIn)
+        logger.debug("VifFileOt : %s", VifFileOt)
+
+        # sort out all the mappings
+        mapping = mapVid + mapAud + mapSub
+        logger.debug("Complete mapping : %s", mapping)
+
+        # create the ffmpeg command
+        ffCommand = ['ffmpeg_g.exe', '-hide_banner', '-y', '-i'] + [VidFileIn]
+        if cli_args.test <> None:
+            ffCommand = ffCommand + ['-t', cli_args.test]
+        
+        ffCommand = ffCommand + mapping + ffVid + ffRescale + ffAud + ffSub + [VifFileOt]
+
+
+        # and run it
+        if cli_args.printmode != True:
+            logger.debug("Complete ffmpeg command : %s", ' '.join(ffCommand))
+            logger.debug("Starting ffmpeg")
+            subprocess.check_call(ffCommand)
+            if cli_args.process == 'replace':
+                logger.debug("Deleting temp file : %s", VidFileIn)
+                os.remove(VidFileIn)
+        else:
+            logger.info("Print mode, no code will be executed")
+            logger.info("Complete ffmpeg command : %s", ' '.join(ffCommand))
+
+
+def RecodeFolder (FolderName):
+    logger.debug("Processing a folder : %s", FolderName)
+    Files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(FolderName) for f in filenames if os.path.splitext(f)[1] in ('.mp4', '.mkv', '.m4v', '.avi', '.mov', '.flv', '.wmv', '.mpg', '.3gp')]
+    Files.sort()
+    #Files.sort(reverse=True)
+    for File in Files:
+        logger.debug("Found in folder %s file %s", FolderName, File)
+        RecodeFile(File)
+
+
+# have we been given a file or a folder
+if os.path.isfile(cli_args.file) == True:
+    RecodeFile(cli_args.file)
+elif os.path.isdir(cli_args.file) == True:
+    RecodeFolder(cli_args.file)
+else:
+	logger.critical("Error not supplied with a valid file or folder : %s", cli_args.file)
+	
---
+++ hevcit/video_functions.py
@@ -0,0 +1,352 @@
+"""video_functions.py
+
+Video functions
+
+Ver  Date        Author      Comment
+1.00 2018-04-02  Chris Cook  Initial version
+"""
+
+import datetime
+
+# import mediainfo
+from pymediainfo import MediaInfo
+
+# import core func for logger
+import core_functions as core
+
+_logger=core.module_null_logger(__name__)
+
+def GetVideoInfo(VidFileIn):
+    """Get all the info about a given media file
+    Parameters : Path to a media file
+    Returns: Four list of dicts describing the video, audio, subtitle and all track info
+    """
+
+    VideoInfo = []
+    AudioInfo = []
+    SubInfo = []
+    AllInfo = []
+
+    counter = - 1
+
+    _logger.info("Reading mediainfo from %s", VidFileIn)
+    try:
+        media_info = MediaInfo.parse(VidFileIn)
+    except OSError:
+        _logger.debug("Using hardcode mediainfo library file")
+        media_info = MediaInfo.parse(VidFileIn, library_file="C:\\Program Files\\MediaInfo\\MediaInfo.dll")
+
+    for track in media_info.tracks:
+        _logger.debug("Processing track %s type %s", track.track_id, track.track_type)
+
+        if track.track_type in ('Audio', 'Video', 'Text'):
+			counter += 1
+
+        # if a video track grab the details
+        if track.track_type == 'Video':
+            # Check the bitrate makes sense by trying to convert it to an integer
+            try:
+                tmpBitRate = int(track.bit_rate)
+                _logger.debug("Bitrate for track %s type %s is %s", track.track_id, track.track_type, tmpBitRate)
+            except:
+                _logger.debug("Bitrate cannot be determined from MediaInfo for track %s type %s", track.track_id, track.track_type)
+                tmpBitRate = 0
+                sys.exit(2)
+
+            # if the file is a varible framerate pretend its 25 fps
+            if track.frame_rate_mode == 'VFR':
+			    track.frame_rate = 25
+
+            # default the duration if there is none
+            if track.duration is None:
+			    track.duration = 0
+
+            # append the information to the arrays
+            VideoInfo.append ({ 'ID': track.track_id, 'Format': track.format, 'BitRate': float(tmpBitRate)/1000, 'Width': track.width, 'Height': track.height, 'FrameRate': track.frame_rate, 'Duration': datetime.timedelta(milliseconds=float(track.duration)), 'ScanType': track.scan_type })
+            AllInfo.append ({ 'Type':'Video', 'ID': track.track_id, 'counter':counter })
+
+        # if an Audio track grab the details
+        elif track.track_type == 'Audio':
+            if track.bit_rate is None:
+                _logger.debug("Bitrate cannot be determined from MediaInfo for track %s type %s", track.track_id, track.track_type)
+                track.bit_rate = 0
+            else:
+                _logger.debug("Bitrate for track %s type %s is %s", track.track_id, track.track_type, tmpBitRate)
+                AudioInfo.append ({ 'ID': track.track_id, 'Format': track.format, 'BitRate': track.bit_rate, 'Channels': track.channel_s })
+			
+            AllInfo.append ({ 'Type':'Audio', 'ID': track.track_id, 'counter':counter })
+
+        # and finally text tracks (subtitles)
+        elif track.track_type == 'Text':
+			SubInfo.append ({ 'ID': track.track_id, 'Format': track.format, 'Language': track.language, 'Default': track.default, 'Forced': track.forced })
+			AllInfo.append ({ 'Type':'Sub', 'ID': track.track_id, 'counter':counter })
+
+    return (VideoInfo, AudioInfo, SubInfo, AllInfo)
+
+
+
+def VideoRescaleCalc(OldWidth, OldHeight, RescaleWidth):
+    """Work out the new size of the a video file
+    Parameters : Oldwidth - Current file width
+               : OldHeight - Current file height
+               : RescaleWidth - Width the file should be rescaled to
+    Returns: List containing the addition to the ffmpeg command to instruct it to rescale
+    """
+    _logger.info("Calculating rescale from %s to %s", OldWidth, RescaleWidth)
+
+    if int(RescaleWidth) < int(OldWidth):
+        NewWidth = RescaleWidth
+        HewHeight = int(round((int(RescaleWidth) * int(OldHeight)) / int(OldWidth), 0))
+        _logger.info("Asked to rescale, will go from %sx%s to %sx%s", OldWidth, OldHeight, RescaleWidth, HewHeight)
+    else:
+        NewWidth = OldWidth
+        HewHeight = OldHeight
+        _logger.info("Asked to rescale, but New Width of %s is greater than start Width of %s so not changing", RescaleWidth, OldWidth)
+
+    ffRescale = ['-vf', 'scale='+str(NewWidth)+':'+str(HewHeight)]
+    return ffRescale, NewWidth
+
+
+def VideoParameters(VideoInfo, TargetBitrate, VideoCodec, AllInfo, LowBitRate):
+    """Work out the video parameters to use in the recode
+    Parameters : VideoInfo - The videoinfo to use (dict)
+               : TargetBitrate - TargetBitrate to use
+               : VideoCodec - Codec to recode to
+               : AllInfo - List of dicts that describes the media
+    Returns:   : mapping - The mapping of input streams to output for ffmpeg
+               : ffVid - The Video section of the ffmpeg command to use for the recode
+               : NewBitrate - The new bitrate that will be used for the encode
+    """
+    _logger.info("Determining Video Parameters")
+    
+    # if we've been asked to pass the video through then just do that
+    if TargetBitrate == "pass":
+        ffVid = ['-c:v', 'copy']
+        _logger.info("Video : %s at %dbps %s long and will be passed through", VideoInfo[0]['Format'], VideoInfo[0]['BitRate'], VideoInfo[0]['Duration'])
+    else:
+        # should we change the framerate?
+        VideoInfo[0]['FrameRate'] = float(VideoInfo[0]['FrameRate'])
+        if VideoInfo[0]['FrameRate'] == 50: TargetFrameRate = 25
+        elif VideoInfo[0]['FrameRate'] == 59.94: TargetFrameRate = 29.97
+        elif VideoInfo[0]['FrameRate'] == 59.88: TargetFrameRate = 29.94
+        elif VideoInfo[0]['FrameRate'] == 59.940: TargetFrameRate = 29.97
+        elif VideoInfo[0]['FrameRate'] == 60: TargetFrameRate = 30
+        elif VideoInfo[0]['FrameRate'] == 100: TargetFrameRate = 25
+        elif VideoInfo[0]['FrameRate'] == 24.97: TargetFrameRate = 25
+        else: TargetFrameRate = VideoInfo[0]['FrameRate']
+
+        if TargetFrameRate <> VideoInfo[0]['FrameRate']:
+            _logger.warning("Got a framerate of %s So resampling with -r %s.", VideoInfo[0]['FrameRate'], TargetFrameRate)
+            Resample = ['-r', str(TargetFrameRate)]
+        else:
+            _logger.info("Got a framerate of %s So no change required", VideoInfo[0]['FrameRate'])
+            Resample = []
+        
+        # Should we deinterlace?
+        if VideoInfo[0]['ScanType'] == 'Interlaced':
+            _logger.warning("Got an interlaced file so will deinterlace")
+            Deinterlace = ['-deinterlace']
+        else:
+            _logger.info("Got a progressive file so no change required")
+            Deinterlace= []
+
+        # calculate the new bitrate
+        if TargetBitrate == "calc":
+            NewBitrate = int(round((((int(VideoInfo[0]['Width']) * int(VideoInfo[0]['Height']) * float(TargetFrameRate)) / 1000000) + 11) * 37, 0))
+        elif TargetBitrate == "calc2":
+            NewBitrate = int(round((((int(VideoInfo[0]['Width']) * int(VideoInfo[0]['Height']) * float(TargetFrameRate)) / 1000000) + 11) * 83, 0))
+        elif TargetBitrate == "calc3":
+            NewBitrate = int(round((((int(VideoInfo[0]['Width']) * int(VideoInfo[0]['Height']) * float(TargetFrameRate)) / 1000000) + 11) * 132, 0))
+        elif TargetBitrate == "half":
+            NewBitrate = int(round(float(VideoInfo[0]['BitRate']) / 2))
+        else:
+            NewBitrate = int(TargetBitrate)
+
+        if VideoCodec == "h264":
+            NewBitrate = NewBitrate * 2.5
+
+        # check the calculated bit is ok
+        if NewBitrate < LowBitRate:
+            _logger.warning("New Bitrate lower than acceptable so using default low value of %d", LowBitRate)
+            NewBitrate = LowBitRate
+
+        # check its lower than the source
+        if NewBitrate >= VideoInfo[0]['BitRate']:
+            _logger.warning("New Bitrate lower than the source so using source bitrate of %d", VideoInfo[0]['BitRate'])
+            NewBitrate = VideoInfo[0]['BitRate']
+
+        # deal with the formats
+        if VideoCodec == "h265":
+            ffcodec = 'nvenc_hevc'
+        elif VideoCodec == "h264":
+            ffcodec = 'nvenc'
+        
+        # create the bits of the ffmpeg command for the video
+        ffVid = ['-c:v', ffcodec, '-b:v', str(NewBitrate)+'k', '-maxrate', '20000k', '-preset', 'hq']
+        # add any extra parts
+        ffVid = ffVid + Resample + Deinterlace
+
+        _logger.info("Video : %s %sx%s at %dk %s long new %s file will be bitrate (%s) %dk", VideoInfo[0]['Format'], VideoInfo[0]['Width'], VideoInfo[0]['Height'], VideoInfo[0]['BitRate'], VideoInfo[0]['Duration'], VideoCodec, TargetBitrate, NewBitrate)
+
+    # calculate the mapping
+    for c in AllInfo:
+        if c['ID'] == VideoInfo[0]['ID']:
+            mapping = ['-map', '0:'+str(c['counter'])]
+    
+    return mapping, ffVid, NewBitrate
+
+
+def AudioParameters(AudioInfo, fileExt, AudioProcess, AllInfo):
+    """Work out the audio parameters to use in the recode
+    Parameters : AudioInfo - The audioInfo to use (dict)
+               : fileExt - File extension of the incoming file
+               : AudioProcess - Process to use for the audio
+               : AllInfo - List of dicts that describes the media
+    Returns:   : mapping - The mapping of input streams to output for ffmpeg
+               : ffAud - The Audio section of the ffmpeg command to use for the recode
+               : formatAud - The video container required to be used by the audio
+    """
+    _logger.info("Determining Audio Parameters")
+
+    # set some constants and defaults
+    format = 'mp4'
+    counter = -1
+    ffAud = []
+    mapping = []
+
+    if not AudioInfo:
+        # no audio
+        _logger.warning("No audio tracks found")
+    else:
+        # fix the channel metadata if required
+        for track in AudioInfo:
+            
+            if track['Channels'] == '8 / 6': track['Channels'] = 8
+            if track['Channels'] == '7 / 6': track['Channels'] = 7
+            try:
+                track['BitRate']=int(track['BitRate'])
+            except:
+                track['BitRate']=int(384000)
+            
+        # calculate the best track
+        _logger.info("Calculating best Audio track")
+        bestTrackID=AudioInfo[0]['ID']
+        bestTrackIx=0
+        channels=int(AudioInfo[0]['Channels'])
+        for track in AudioInfo:
+            _logger.debug("Audio track %s, %s Channels, %sk %s", track['ID'], track['Channels'], track['BitRate']/1000, track['Format'])
+            counter += 1
+            if int(track['Channels']) > channels:
+                bestTrackID=AudioInfo[0]['ID']
+                bestTrackIx=counter
+
+        _logger.info("Best track ID : %s, Index : %s which is %sk %s channel %s", bestTrackID, bestTrackIx, AudioInfo[bestTrackIx]['BitRate']/1000, AudioInfo[bestTrackIx]['Channels'], AudioInfo[bestTrackIx]['Format'])
+        
+        # just the one track required
+        if AudioProcess in ("one", "aac", "64k"):
+            if AudioInfo[bestTrackIx]['Channels'] >= 6:
+                AudioBitrate = '384k'
+                AudioCodec = 'ac3'
+                format = 'mkv'
+            else:
+                AudioBitrate = '128k'
+                AudioCodec = 'libfdk_aac'
+                format = 'mp4'
+
+            if AudioProcess == '64k':
+                AudioBitrate = '64k'
+
+            ffAud = ['-c:a:0', AudioCodec, '-b:a:0', AudioBitrate, '-ar:0', '48000']
+
+            # calculate the mapping
+            for c in AllInfo:
+                if c['ID'] == AudioInfo[bestTrackIx]['ID']:
+                    mapping = ['-map', '0:'+str(c['counter'])]
+
+            _logger.info("Keeping Audio track :%s %sk %s channel %s, will recode to %s %s", str(AudioInfo[bestTrackIx]['ID'] - 1), AudioInfo[bestTrackIx]['BitRate']/1000, AudioInfo[bestTrackIx]['Channels'], AudioInfo[bestTrackIx]['Format'], AudioBitrate, AudioCodec)
+
+        # pass throught the best track
+        elif AudioProcess == "passbest":
+            ffAud = ['-c:a:0', 'copy']
+            format = 'mkv'
+			
+            # calculate the mapping
+            for c in AllInfo:
+                if c['ID'] == AudioInfo[bestTrackIx]['ID']:
+                    mapping = ['-map', '0:'+str(c['counter'])]
+					
+            _logger.info("Keeping Audio track %s %sk %s channel %s and passing it through", str(AudioInfo[counter]['ID'] - 1), AudioInfo[counter]['BitRate']/1000, AudioInfo[counter]['Channels'], AudioInfo[counter]['Format'])
+            
+        elif AudioProcess == "passall":
+            counter = 0
+            format = 'mkv'
+            for track in AudioInfo:
+                ffAud = ffAud + ['-c:a:'+ str(counter), 'copy']
+                _logger.info("Keeping Audio track %s %sk %s channel %s and passing it through", str(AudioInfo[counter]['ID'] - 1), AudioInfo[counter]['BitRate']/1000, AudioInfo[counter]['Channels'], AudioInfo[counter]['Format'])
+            
+            # calculate the mapping
+            for c in AllInfo:
+                if c['ID'] == AudioInfo[counter]['ID']:
+                    mapping += ['-map', '0:'+str(c['counter'])]
+            counter += 1
+        
+        elif AudioProcess == "all":
+            counter = 0
+            for track in AudioInfo:
+                if AudioInfo[counter]['Channels'] >= 6:
+                    AudioBitrate = '384k'
+                    AudioCodec = 'ac3'
+                    format = 'mkv'
+                else:
+                    AudioBitrate = '128k'
+                    AudioCodec = 'libfdk_aac'
+                
+                # if more than one track then we have to use mkv
+                if counter > 0 and format != 'mkv':
+                    format = 'mkv'
+
+                ffAud += ['-c:a:' + str(counter), AudioCodec, '-b:a:' + str(counter), AudioBitrate, '-ar:' + str(counter), '48000']
+
+                _logger.info("Keeping Audio track :%s %sk %s channel %s, will recode to %s %s", str(AudioInfo[counter]['ID'] - 1), AudioInfo[counter]['BitRate']/1000, AudioInfo[counter]['Channels'], AudioInfo[counter]['Format'], AudioBitrate, AudioCodec)
+            
+            # calculate the mapping
+            for c in AllInfo:
+                if c['ID'] == AudioInfo[counter]['ID']:
+                    mapping += ['-map', '0:'+str(c['counter'])]
+                    counter += 1
+
+    return (mapping, ffAud, format)
+
+
+def SubParameters(SubInfo):
+    """Work out the subtitle parameters to use in the recode
+    Parameters : SubInfo - The SubInfo to use (dict)
+    Returns:   : mapping - The mapping of input streams to output for ffmpeg
+               : ffSub - The Subtitle section of the ffmpeg command to use for the recode
+               : formatSub - The video container required to be used by the subtitles
+    """
+    ffSub=[]
+    mapping=[]
+    counter = 0
+    format = 'mp4'
+
+    for track in SubInfo:
+        if track['Language'] == 'en' or track['Forced'] == 'Yes':
+            #or track['Default'] == 'Yes' 
+            # subtitle is english, default or forced, so we'll keep it
+            mapping = mapping + ['-map', '0:' + str(track['ID'] - 1)]
+            if track['Format'] in ("ASS", "UTF-8", "SSA"):
+                # a format we can recode so we will
+                ffSub = ffSub + ['-c:s:'+str(counter), 'ass']
+                format = 'mkv'
+                _logger.info("Keeping Subtitle track %s %s %s, will recode to Advanced SubStation Alpha", str(track['ID'] - 1), track['Language'], track['Format'])
+            if track['Format'] in ("PGS", "VobSub"):
+                # a format we can't code, so we'll just pass it through
+                ffSub = ffSub + ['-c:s:'+str(counter), 'copy']
+                format = 'mkv'
+                _logger.info("Keeping Subtitle track %s %s %s, will be passed through unchanged", str(track['ID'] - 1), track['Language'], track['Format'])
+        counter += 1
+
+    return (mapping, ffSub, format)
+
+
